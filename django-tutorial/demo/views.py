39e906ca737e445bb3ef3894c73c44c8e03b43ea
Jeremy Barnes <jeremy.barnes@braingu.com>
2025-03-24 09:20:10 -0400

    updates to django project for object queries

--- django-tutorial/demo/views.py
+++ django-tutorial/demo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
-# Create your views here.
+class Another(View):
+
+    book = Book.objects.get(id=2)
+    output = ''
+    #for book in books:
+    output += f"We have {book.title} with ID {book.id}<br />"
+
+    def get(self, request: object) -> HttpResponse:
+        return HttpResponse(self.output)
+
+def first(request) -> HttpResponse:
+    return HttpResponse("First message from views")
